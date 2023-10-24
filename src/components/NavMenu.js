62c3d305eb40b136c4e2f9d899d24f6e532cea49
Robin Kelsick II <robinkelsick@gmail.com>
2023-10-23 21:44:29 -0400

    added react icons package

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export const NavMenu = () => {
-  return <div className='navbar bg-neutral'></div>;
+  return (
+    <div className='navbar bg-neutral'>
+      <div className='navbar-start'>
+        <label tabIndex={0} className='btn btn-ghost btn-circle'></label>
+      </div>
+    </div>
+  );
 };
