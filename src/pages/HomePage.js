c0e74a45447ddc4cfcfd6c27547d8f7629207c92
Robin Kelsick II <robinkelsick@gmail.com>
2023-10-25 21:32:24 -0400

    styling the home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,8 +5,11 @@ export const HomePage = () => {
   return (
     <div className='w-full h-screen'>
       <NavMenu />
-      <div className='flex flex-col my-20 lg:flex-row'>
-        <div></div>
+      <div className='flex flex-col mt-5 lg:flex-row'>
+        <div className='w-1/2 h-screen'></div>
+        <div className='flex justify-center w-1/2 h-screen py-8 mt-auto'>
+          <h1 className='text-4xl'>You Decide where go next</h1>
+        </div>
       </div>
     </div>
   );
