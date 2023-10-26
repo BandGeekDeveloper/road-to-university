07993c9cc57dd0143090429e0eec82485784e0f6
Robin Kelsick II <robinkelsick@gmail.com>
2023-10-26 18:34:09 -0400

    added buttons on home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,9 +6,24 @@ export const HomePage = () => {
     <div className='w-full h-screen'>
       <NavMenu />
       <div className='flex flex-col mt-5 lg:flex-row'>
-        <div className='w-1/2 h-screen'></div>
-        <div className='flex justify-center w-1/2 h-screen py-8 mt-auto'>
-          <h1 className='text-4xl'>You Decide where go next</h1>
+        <div className='w-full h-screen lg:w-1/2'></div>
+        <div className='flex flex-col justify-center items-center w-full lg:w-1/2 h-screen py-8 m-auto'>
+          <h1 className='text-3xl lg:text-4xl'>You Decide where go next</h1>
+          <p className='mt-8 text-center'>
+            As you stand at the crossroads of your future, remember that this is
+            your journey, and you have the power to shape it in ways you can't
+            even imagine yet. Embrace the uncertainty, follow your passions, and
+            trust in your abilities. The world is full of opportunities waiting
+            for you to explore and create your own path.
+          </p>
+          <div className='mt-8 flex space-x-10'>
+            <button className='btn btn-primary shadow-lg'>
+              Begin your Journey
+            </button>
+            <button className='btn btn-accent shadow-lg'>
+              Begin your Journey
+            </button>
+          </div>
         </div>
       </div>
     </div>
